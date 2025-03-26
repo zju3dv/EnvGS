569d4b8a8a1266e8206d4a043eb22da2e5753a04
Tao Xie <141353673+xbillowy@users.noreply.github.com>
2025-03-26 11:12:09 +0800

    Update train_nerf-casting.sh

--- scripts/envgs/train_nerf-casting.sh
+++ scripts/envgs/train_nerf-casting.sh
@@ -1,7 +1,7 @@
 # Training script for nerf-casting dataset
 # Including scenes: compact, grinder, hatchback, toaster
 
-ecb-train -c configs/exps/envgs/nerf-casting/envgs_compact.yaml exp_name=envgs/nerf-casting/envgs_compact
-ecb-train -c configs/exps/envgs/nerf-casting/envgs_grinder.yaml exp_name=envgs/nerf-casting/envgs_grinder
-ecb-train -c configs/exps/envgs/nerf-casting/envgs_hatchback.yaml exp_name=envgs/nerf-casting/envgs_hatchback
-ecb-train -c configs/exps/envgs/nerf-casting/envgs_toaster.yaml exp_name=envgs/nerf-casting/envgs_toaster
+evc-train -c configs/exps/envgs/nerf-casting/envgs_compact.yaml exp_name=envgs/nerf-casting/envgs_compact
+evc-train -c configs/exps/envgs/nerf-casting/envgs_grinder.yaml exp_name=envgs/nerf-casting/envgs_grinder
+evc-train -c configs/exps/envgs/nerf-casting/envgs_hatchback.yaml exp_name=envgs/nerf-casting/envgs_hatchback
+evc-train -c configs/exps/envgs/nerf-casting/envgs_toaster.yaml exp_name=envgs/nerf-casting/envgs_toaster
