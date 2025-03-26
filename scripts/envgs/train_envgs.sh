0cc4df9a2d4f825709a8b98f3a8d3375a5cff7a7
Tao Xie <141353673+xbillowy@users.noreply.github.com>
2025-03-26 11:11:53 +0800

    update: train_envgs.sh

--- scripts/envgs/train_envgs.sh
+++ scripts/envgs/train_envgs.sh
@@ -1,5 +1,5 @@
 # Training script for envgs dataset
 # Including scenes: audi, dog
 
-ecb-train -c configs/exps/envgs/envgs/envgs_audi.yaml exp_name=envgs/envgs/envgs_audi
-ecb-train -c configs/exps/envgs/envgs/envgs_dog.yaml exp_name=envgs/envgs/envgs_dog
+evc-train -c configs/exps/envgs/envgs/envgs_audi.yaml exp_name=envgs/envgs/envgs_audi
+evc-train -c configs/exps/envgs/envgs/envgs_dog.yaml exp_name=envgs/envgs/envgs_dog
